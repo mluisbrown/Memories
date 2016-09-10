15d6151e96f939ad0f478d3f604b9adcbfbe10f4
Michael Brown <me@michael-brown.net>
2016-09-10 16:57:57 +0100

    Added back calls Fabric :/

--- Memories/AppDelegate.swift
+++ Memories/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]?) -> Bool {
+        Fabric.with([Crashlytics.self])
+
         UserDefaults.standard.register(defaults: [NotificationManager.Key.notificationTime : 1000,
             NotificationManager.Key.hasPromptedForUserNotifications : false,
             NotificationManager.Key.notificationsEnabled: false])
